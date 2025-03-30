1d670787d0505985e2bafef4f4a43afd61de6315
CongNhut1210 <nhuttran12102000@gmail.com>
2025-03-30 10:26:40 +0700

    z

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
-    public $fillable = ['title','description','long_description','completed']
+    public $fillable = ['title','description','long_description','completed'];
+
+    public function toggleCompleted(){
+        $this->completed = !$this->completed;
+        $this->save();
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,42 @@
 @extends('layouts.app')
-@section('title','To-Do-app')
+@section('title', 'To Do App')
 @section('content')
-    <a href="{{route('tasks.create')}}"> +Create Task
-    <table>
+    <div class="mb-4">
+        <a class="font-medium text-gray-700 underline decoration-blue-500 "
+         href="{{ route('tasks.create')}}">+ Create Task</a>
+    </div>
+    <table class='"border-separate border-spacing-2 border border-gray-400 dark:border-gray-500"'>
+
         <tr>
             <th>ID</th>
             <th>Title</th>
+            {{-- <th>Description</th>
+            <th>Long Description</th> --}}
             <th>Status</th>
             <th>Action</th>
         </tr>
             @if (count($tasks)>0)
                 @foreach ($tasks as $task )
                 <tr>
-                <!--Phan tu cua 1 list -->
                     <td>{{$task->id}}</td>
                     <td>{{$task->title}}</td>
                     <td>{{$task->completed==true?"Completed":"Uncompleted"}}</td>
-                    <td><a href="{{route('tasks.detail',['id'=>$task->id])}}">Detail</a>
-                        <a href="{{route('tasks.edit',['id'=>$task->id])}}">Edit</a>
+                    <td>
+                    <div class="inline-flex">
+                        <form action="{{route('tasks.toggle-completed', ['id'=>$task->id])}}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <button type="submit" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l" >{{$task->completed!=true?'Completed':'Uncompleted'}}</button>
+                        </form>
+                        <a href="{{route('tasks.detail',['id'=>$task->id])}}" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">Detail</a>
+                        <a href="{{route('tasks.edit',['id'=>$task->id])}}" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">Edit</a>
                         <form action="{{route('tasks.delete',['id'=>$task->id])}}" method="POST">
                             @csrf
                             @method('DELETE')
-                            <button type="submit">Delete</button>
+                            <button type="submit" class="bg-red-300 hover:bg-blue-400 text-gray-900000 font-bold py-2 px-4 rounded-l">Delete</button>
                         </form>
-                    </td>
+                    </div>
+                </td>
                 </tr>
                 @endforeach
             @else
@@ -32,4 +45,7 @@
                 </tr>
             @endif
     </table>
+    @if ($tasks->count())
+    <nav>{{$tasks->links()}}</nav>
+    @endif
 @endsection
